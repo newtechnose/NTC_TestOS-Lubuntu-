7842cd6a56307a50f4b391c4d8b9b0f3a85ebc5a
newtechnose <yoshiki.nose@newtech.co.jp>
2025-10-31 20:29:59 +0900

    Ver2.00.02_Update_3

--- shell/minicom.sh
+++ shell/minicom.sh
@@ -1,2 +1,2 @@
 zenity --info --width 800 --title="COMポートテスト" --text="製品とテスト用PCをRS232Cケーブルで接続し、minicomが起動したら互いに入力した文字が出力されているか確認してください。\n\n終了するときはCtrl + a を押して x を押すと終了する画面が出てくるので、Enterキーを押し、終了してください。\n※ウィンドウの✕ボタンでは終了しないでください。"
-qterminal -e "bash -c 'minicom; bash'"
+qterminal -e "bash -c 'sudo minicom; bash'"
